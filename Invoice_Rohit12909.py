03b2a5ba6391c87ba92ddcf25e0d7c1315b19b6f
Rohit12909 <71101658+Rohit12909@users.noreply.github.com>
2020-11-11 17:49:37 -0500

    Add files via upload

---
+++ Invoice_Rohit12909.py
@@ -0,0 +1,63 @@
+class Invoice:
+    def __init__(self):
+        self.items = {}
+
+    def addProduct(self, qnt, price, discount):
+        self.items['qnt'] = qnt
+        self.items['unit_price'] = price
+        self.items['discount'] = discount
+
+        return self.items
+
+    def totalImpurePrice(self, products):
+        total_impure_price = 0
+        for k, v in products.items():
+            total_impure_price += float(v['unit_price']) * int(v['qnt'])
+        total_impure_price = round(total_impure_price, 2)
+        return total_impure_price
+
+    def totalDiscount(self, products):
+        total_discount = 0
+        for k, v in products.items():
+            total_discount += (int(v['qnt']) * float(v['unit_price'])) * float(v['discount']) / 100
+        total_discount = round(total_discount, 2)
+        self.total_discount = total_discount
+        return total_discount
+
+    def totalPurePrice(self, products):
+        total_pure_price = self.totalImpurePrice(products) - self.totalDiscount(products)
+        return total_pure_price
+
+    def inputAnswer(self, input_value):
+        while True:
+            userInput = input(input_value)
+            if userInput in ['y', 'n']:
+                return userInput
+            print("y or n! Try again.")
+
+    def inputNumber(self, input_value):
+        while True:
+            try:
+                userInput = float(input(input_value))
+            except ValueError:
+                print("Not a number! Try again.")
+                continue
+            else:
+                return userInput
+
+    def totalQuantity(self, products):
+        total_quantity = 0
+        for k, v in products.items():
+            total_quantity += (int(v['qnt']))
+        total_quantity = round(total_quantity, 2)
+        self.total_quantity = total_quantity
+        return total_quantity
+
+    def totalTax(self, products):
+        total_tax = 0
+        for k, v in products.items():
+            total_tax += (float(v['tax'])) * (int(v['qnt']))
+        total_tax = round(total_tax, 2)
+        self.total_tax = total_tax
+        return total_tax
+
